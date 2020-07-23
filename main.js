08e2609aba9aa2aa5792852d6a0371515ea432b7
Carmen Odimba <carmen.odimba@laposte.net>
2020-07-24 00:15:07 +0200

    today

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
-// Dynamic background: light during the day 8:00 to 20:00 GMT+1
+// Topnav
+const advertise_btn = document.getElementById('advertise');
+const login_btn = document.getElementById('login');
+const help_btn = document.getElementById('help');
+// Main navbar
+const searchbar = document.querySelector('.main-searchbar');
+const shops_btn = document.getElementById('shops-btn');
+const restaurants_btn = document.getElementById('restaurants-btn')
+const clubs_btn = document.getElementById('clubs-btn');
+const save_btn = document.getElementById('save-btn');
 
-// CSS or JS wave effect on click
+
+
+// API
+
+
+// Find afro shops in all German cities
+
+
+
+// Find all African restaurants in all German cities
+
+
+
+// Find all African clubs in all German cities
+
